6ccd9a5bbbf8d9cc662a51f75690880233e8213b
Aman <amangrand@gmail.com>
2021-01-29 12:41:46 +0530

    Done

---
+++ maps/src/index.ts
@@ -0,0 +1,22 @@
+// import { User } from './User'
+// import { Company } from './Company'
+
+// const user= new User();
+// const company= new Company();
+// console.log(user);
+// console.log(company);
+// console.log(google)
+console.log('hi')
+
+//  new google.maps.Map(document.getElementById('map')!,{
+//      zoom:1,
+//      center:{
+//          lat:0,
+//          lng:0
+//      } 
+//  }); 
+
+ 
+
+
+
