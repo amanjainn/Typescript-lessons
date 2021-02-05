4eda043a5001b55ac6b0899e050a453068c27169
Aman <amangrand@gmail.com>
2021-02-05 19:31:30 +0530

    redux ts

---
+++ my-ss/src/RepositoriesList.tsx
@@ -0,0 +1,25 @@
+import {useState} from 'react';
+import {useActions} from '../src/hooks/useActions'
+import {useSelector} from 'react-redux';
+
+
+
+const RepositoriesList:React.FC=()=>{
+    const [term,setTerm]=useState('');
+    const { searchRepositories }=useActions();
+    const state=useSelector((state)=>state);
+    console.log(state)
+
+        const onSubmit=(event:React.FormEvent<HTMLFormElement>)=>{
+            event.preventDefault(); 
+            searchRepositories(term) ;
+        }
+        return <div>
+            <form onSubmit={onSubmit}>
+                <input value={term} onChange={e=>setTerm(e.target.value)}  />
+                <button>Search</button>
+            </form>
+        </div>
+}
+
+export default RepositoriesList;
